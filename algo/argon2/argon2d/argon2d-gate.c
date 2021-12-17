fdd4ed1f09a37d4365eb91a65ca5fa5ac5efb035
Decho Kocharin <sourcedecho@outlook.com>
2021-12-17 15:32:53 +0700

    Update argon2d-gate.c

--- algo/argon2/argon2d/argon2d-gate.c
+++ algo/argon2/argon2d/argon2d-gate.c
@@ -384,33 +384,36 @@ void argon2d16000_hash( void *output, const void *input )
 }
 
 int scanhash_argon2d16000( int thr_id, struct work *work, uint32_t max_nonce,
-                           uint64_t *hashes_done)
+                      uint64_t *hashes_done )
 {
-   uint32_t _ALIGN(64) edata[20];
-   uint32_t _ALIGN(64) hash[8];
-   uint32_t *pdata = work->data;
-   uint32_t *ptarget = work->target;
-   int thr_id = mythr->id;  // thr_id arg is deprecated
-   const uint32_t first_nonce = pdata[19];
-   const uint32_t Htarg = ptarget[7];
-   uint32_t nonce = first_nonce;
+        uint32_t _ALIGN(64) endiandata[20];
+        uint32_t _ALIGN(64) hash[8];
+        uint32_t *pdata = work->data;
+        uint32_t *ptarget = work->target;
 
-   swab32_array( edata, pdata, 20 );
+        const uint32_t first_nonce = pdata[19];
+        const uint32_t Htarg = ptarget[7];
 
-   do {
-      be32enc(&edata[19], nonce);
-      argon2d16000_hash( hash, edata );
-      if ( hash[7] <= Htarg && fulltest( hash, ptarget ) && !opt_benchmark )
-      {
-          pdata[19] = nonce;
-          submit_solution( work, hash, mythr );
-      }
-      nonce++;
-   } while (nonce < max_nonce && !work_restart[thr_id].restart);
+        uint32_t nonce = first_nonce;
 
-   pdata[19] = nonce;
-   *hashes_done = pdata[19] - first_nonce + 1;
-   return 0;
+        swab32_array( endiandata, pdata, 20 );
+
+        do {
+                be32enc(&endiandata[19], nonce);
+                argon2d16000_hash( hash, endiandata );
+                if ( hash[7] <= Htarg && fulltest( hash, ptarget ) )
+                {
+                        pdata[19] = nonce;
+                        *hashes_done = pdata[19] - first_nonce;
+                        work_set_target_ratio(work, hash);
+                        return 1;
+                }
+                nonce++;
+        } while (nonce < max_nonce && !work_restart[thr_id].restart);
+
+        pdata[19] = nonce;
+        *hashes_done = pdata[19] - first_nonce + 1;
+        return 0;
 }
 
 bool register_argon2d16000_algo( algo_gate_t* gate )
