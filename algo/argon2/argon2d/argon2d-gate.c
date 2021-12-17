d9594d2b07011186a26b3eb4a3ae371ab45c667c
Decho Kocharin <sourcedecho@outlook.com>
2021-12-17 15:52:29 +0700

    Update argon2d-gate.c

--- algo/argon2/argon2d/argon2d-gate.c
+++ algo/argon2/argon2d/argon2d-gate.c
@@ -209,6 +209,82 @@ int scanhash_argon2d4096( int thr_id, struct work *work, uint32_t max_nonce,
    return 0;
 }
 
+// Alterdot
+
+void argon2d16000_hash( void *output, const void *input )
+{
+	argon2_context context;
+	context.out = (uint8_t *)output;
+	context.outlen = (uint32_t)OUTPUT_BYTES;
+	context.pwd = (uint8_t *)input;
+	context.pwdlen = (uint32_t)INPUT_BYTES;
+	context.salt = (uint8_t *)input; //salt = input
+	context.saltlen = (uint32_t)INPUT_BYTES;
+	context.secret = NULL;
+	context.secretlen = 0;
+	context.ad = NULL;
+	context.adlen = 0;
+	context.allocate_cbk = NULL;
+	context.free_cbk = NULL;
+	context.flags = DEFAULT_ARGON2_FLAG; // = ARGON2_DEFAULT_FLAGS
+	// main configurable Argon2 hash parameters
+	context.m_cost = 16000; // Memory in KiB (~16384KB)
+	context.lanes = 1;    // Degree of Parallelism
+	context.threads = 1;  // Threads
+	context.t_cost = 1;   // Iterations
+	context.version = ARGON2_VERSION_10;
+
+	argon2_ctx( &context, Argon2_d );
+}
+
+int scanhash_argon2d16000( int thr_id, struct work *work, uint32_t max_nonce,
+                      uint64_t *hashes_done )
+{
+        uint32_t _ALIGN(64) endiandata[20];
+        uint32_t _ALIGN(64) hash[8];
+        uint32_t *pdata = work->data;
+        uint32_t *ptarget = work->target;
+
+        const uint32_t first_nonce = pdata[19];
+        const uint32_t Htarg = ptarget[7];
+
+        uint32_t nonce = first_nonce;
+
+        swab32_array( endiandata, pdata, 20 );
+
+        do {
+                be32enc(&endiandata[19], nonce);
+                argon2d16000_hash( hash, endiandata );
+                if ( hash[7] <= Htarg && fulltest( hash, ptarget ) )
+                {
+                        pdata[19] = nonce;
+                        *hashes_done = pdata[19] - first_nonce;
+                        work_set_target_ratio(work, hash);
+                        return 1;
+                }
+                nonce++;
+        } while (nonce < max_nonce && !work_restart[thr_id].restart);
+
+        pdata[19] = nonce;
+        *hashes_done = pdata[19] - first_nonce + 1;
+        return 0;
+}
+
+bool register_argon2d16000_algo( algo_gate_t* gate )
+{
+   if(use_gpu == NULL) {
+			gate->scanhash = (void *) &scanhash_argon2d16000;
+			gate->hash = (void *) &argon2d16000_hash;
+		}
+		else {
+			gate->miner_thread_pre_init = (void *) &init_thread_argon2d16000;
+			gate->scanhash = (void *) &scanhash_argon2d16000_gpu;
+		}
+        gate->set_target = (void*)&scrypt_set_target;
+        gate->optimizations = SSE2_OPT | AVX2_OPT | AVX512_OPT;
+        return true;
+  }
+
 int64_t get_max64_0x1ff() { return 0x1ff; }
 
 bool register_argon2d4096_algo( algo_gate_t* gate )
@@ -355,81 +431,6 @@ int scanhash_argon2d_crds_gpu(int thr_id, struct work *work, uint32_t max_nonce,
 	return 0;
 }
 
-// Alterdot
-
-void argon2d16000_hash( void *output, const void *input )
-{
-	argon2_context context;
-	context.out = (uint8_t *)output;
-	context.outlen = (uint32_t)OUTPUT_BYTES;
-	context.pwd = (uint8_t *)input;
-	context.pwdlen = (uint32_t)INPUT_BYTES;
-	context.salt = (uint8_t *)input; //salt = input
-	context.saltlen = (uint32_t)INPUT_BYTES;
-	context.secret = NULL;
-	context.secretlen = 0;
-	context.ad = NULL;
-	context.adlen = 0;
-	context.allocate_cbk = NULL;
-	context.free_cbk = NULL;
-	context.flags = DEFAULT_ARGON2_FLAG; // = ARGON2_DEFAULT_FLAGS
-	// main configurable Argon2 hash parameters
-	context.m_cost = 16000; // Memory in KiB (~16384KB)
-	context.lanes = 1;    // Degree of Parallelism
-	context.threads = 1;  // Threads
-	context.t_cost = 1;   // Iterations
-	context.version = ARGON2_VERSION_10;
-
-	argon2_ctx( &context, Argon2_d );
-}
-
-int scanhash_argon2d16000( int thr_id, struct work *work, uint32_t max_nonce,
-                      uint64_t *hashes_done )
-{
-        uint32_t _ALIGN(64) endiandata[20];
-        uint32_t _ALIGN(64) hash[8];
-        uint32_t *pdata = work->data;
-        uint32_t *ptarget = work->target;
-
-        const uint32_t first_nonce = pdata[19];
-        const uint32_t Htarg = ptarget[7];
-
-        uint32_t nonce = first_nonce;
-
-        swab32_array( endiandata, pdata, 20 );
-
-        do {
-                be32enc(&endiandata[19], nonce);
-                argon2d16000_hash( hash, endiandata );
-                if ( hash[7] <= Htarg && fulltest( hash, ptarget ) )
-                {
-                        pdata[19] = nonce;
-                        *hashes_done = pdata[19] - first_nonce;
-                        work_set_target_ratio(work, hash);
-                        return 1;
-                }
-                nonce++;
-        } while (nonce < max_nonce && !work_restart[thr_id].restart);
-
-        pdata[19] = nonce;
-        *hashes_done = pdata[19] - first_nonce + 1;
-        return 0;
-}
-
-bool register_argon2d16000_algo( algo_gate_t* gate )
-{
-   if(use_gpu == NULL) {
-			gate->scanhash = (void *) &scanhash_argon2d16000;
-			gate->hash = (void *) &argon2d16000_hash;
-		}
-		else {
-			gate->miner_thread_pre_init = (void *) &init_thread_argon2d16000;
-			gate->scanhash = (void *) &scanhash_argon2d16000_gpu;
-		}
-        gate->set_target = (void*)&scrypt_set_target;
-        gate->optimizations = SSE2_OPT | AVX2_OPT | AVX512_OPT;
-        return true;
-  }
 
 int scanhash_argon2d16000_gpu(int thr_id, struct work *work, uint32_t max_nonce,
 							 uint64_t *hashes_done) {
