d7458718c135036e59a3c818d1cd617a97cce0d0
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2022-08-21 17:21:55 +0200

    Update argon2d-gpu-gate.h

--- algo/argon2/argon2d/argon2d-gpu-gate.h
+++ algo/argon2/argon2d/argon2d-gpu-gate.h
@@ -24,6 +24,7 @@ typedef struct argon2_gpu_hasher_thread_ {
 DLLEXPORT int check_gpu_capability(char *_use_gpu, char *_gpu_id, int _gpu_batch_size, int threads);
 DLLEXPORT argon2_gpu_hasher_thread *get_gpu_thread_data(int thr_id);
 DLLEXPORT void gpu_argon2_raw_hash(argon2_gpu_hasher_thread *thread_data);
+DLLEXPORT bool init_thread_argon2d16000_gpu(int thr_id);
 DLLEXPORT bool init_thread_argon2d4096_gpu(int thr_id);
 DLLEXPORT bool init_thread_argon2d_dyn_gpu(int thr_id);
 DLLEXPORT bool init_thread_argon2d_crds_gpu(int thr_id);
